a7c1f7578c82bb0ae9d852af738a6fde0c3c8ddf
fandressouza <contact@programming-mentor.com>
2020-06-22 07:49:54 +0100

    Added the templates for the views, edited pages/urls.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'pages.apps.PagesConfig', #added this item
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
